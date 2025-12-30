58477d4f7787a006610eec41b6552fe361e6a445
Maik Tappe <maik.tappe82@googlemail.com>
2025-12-30 10:18:40 +0100

    The final Template

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import webview
 
+class Api:
+	def printit(self):
+		print("print it")
+
 def main():
-    window = webview.create_window(
-        "Python → JS",
-        url="index.html",
-        width=1000,
-        height=700
-    )
+	api = Api()
+	window = webview.create_window(
+		"Python -> Js",
+		url="ui/index.html",
+		width=1000,
+		height=700,
+		js_api=api
+	)
 
-    def after_start():
-        # JS-Funktion aufrufen (und Rückgabewert bekommen)
-        result = window.evaluate_js("window.setStatus('Hallo aus Python!')")
-        print("Python bekam zurück:", result)
+	def after_start():
+		window.evaluate_js("console.log('Console: Grüße aus Python')")
 
-        # Du kannst auch direkt JS ausführen:
-        window.evaluate_js("console.log('Console: Grüße aus Python')")
+	webview.start(after_start, debug=True)
 
-    webview.start(after_start, debug=True)
-
-if __name__ == "__main__":
-    main()
+if __name__=="__main__":
+	main()
